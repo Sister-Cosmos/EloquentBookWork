de0ce7892030895ce163967eba358d69aeacaa0a
Owen Gerlach <owengerlach04@gmail.com>
2024-10-01 12:37:17 -0400

    Chapters 5 & 6 have exercises now

--- Chapter-5/Chapter5.js
+++ Chapter-5/Chapter5.js
@@ -1,1 +1,45 @@
-// Hello there james, im a tiny tiny tiny bit behind :) this file will have cool examples from the book sometime on wednesday. Don't kill me :)
+//Flattening
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce((flat, current) => flat.concat(current), []));
+
+//Your Own Loop
+function loop(start, test, update, body) {
+    for (let value = start; test(value); value = update(value)) {
+      body(value);
+    }
+  }
+  
+  loop(3, n => n > 0, n => n - 1, console.log);
+
+//EveryThing
+function every(array, predicate) {
+    for (let element of array) {
+      if (!predicate(element)) return false;
+    }
+    return true;
+  }
+  
+  function every2(array, predicate) {
+    return !array.some(element => !predicate(element));
+  }
+  console.log(every([1, 3, 5], n => n < 10));
+  console.log(every([2, 4, 16], n => n < 10));
+  console.log(every([], n => n < 10));
+
+//dominant Writting direction
+function dominantDirection(text) {
+    let counted = countBy(text, char => {
+      let script = characterScript(char.codePointAt(0));
+      return script ? script.direction : "none";
+    }).filter(({name}) => name != "none");
+  
+    if (counted.length == 0) return "ltr";
+  
+    return counted.reduce((a, b) => a.count > b.count ? a : b).name;
+  }
+  
+  console.log(dominantDirection("Hello!"));
+  console.log(dominantDirection("Hey, مساء الخير"));
+
+  
--- Chapter-6/Chapter6.js
+++ Chapter-6/Chapter6.js
@@ -1,1 +1,112 @@
-// Hello there james, im a tiny tiny tiny bit behind :) this file will have cool examples from the book sometime on wednesday. Don't kill me :)
+//A vector Type
+
+class Vec {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+    plus(other) {
+      return new Vec(this.x + other.x, this.y + other.y);
+    }
+    minus(other) {
+      return new Vec(this.x - other.x, this.y - other.y);
+    }
+    get length() {
+      return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+  }
+
+  console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+  console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+  console.log(new Vec(3, 4).length);
+
+
+
+//Groups
+
+class GrouP {
+    #members = [];
+  
+    add(value) {
+      if (!this.has(value)) {
+        this.#members.push(value);
+      }
+    }
+    delete(value) {
+      this.#members = this.#members.filter(v => v !== value);
+    }
+    has(value) {
+      return this.#members.includes(value);
+    } 
+    static from(collection) {
+      let grouP = new GrouP;
+      for (let value of collection) {
+        grouP.add(value);
+      }
+      return grouP;
+    }
+  }
+  let grouP = GrouP.from([10, 20]);
+  console.log(grouP.has(10));
+  console.log(grouP.has(30));
+  grouP.add(10);
+  grouP.delete(10);
+  console.log(grouP.has(10));
+
+
+
+//iterable groups
+
+class Group {
+    #members = [];
+
+    add(value) {
+      if (!this.has(value)) {
+        this.#members.push(value);
+      }
+    }
+    delete(value) {
+      this.#members = this.#members.filter(v => v !== value);
+    }
+    has(value) {
+      return this.#members.includes(value);
+    }
+    static from(collection) {
+      let group = new Group;
+      for (let value of collection) {
+        group.add(value);
+      }
+      return group;
+    }
+    [Symbol.iterator]() {
+      return new GroupIterator(this.#members);
+    }
+  }
+  class GroupIterator {
+    #members;
+    #position;
+
+    constructor(members) {
+      this.#members = members;
+      this.#position = 0;
+    }
+    next() {
+      if (this.#position >= this.#members.length) {
+        return {done: true};
+      } else {
+        let result = {value: this.#members[this.#position],
+                      done: false};
+        this.#position++;
+        return result;
+      }
+    }
+  }
+  for (let value of Group.from(["a", "b", "c"])) {
+    console.log(value);
+  }
+
+
+//Borrowing a method
+let map = {one: true, two: true, hasOwnProperty: true};
+
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));
